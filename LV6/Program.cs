421cdbdf31ca566ee9bdadc1ab367e9ae714b9ee
TheDegman <72929923+TheDegman@users.noreply.github.com>
2021-12-14 15:43:05 +0100

    Update Program.cs

--- LV6/Program.cs
+++ LV6/Program.cs
@@ -77,3 +77,80 @@ namespace LV6
         }
     }
 }
+
+
+
+//using System;
+
+
+//namespace LV6
+//{
+//    class Program
+//    {
+        
+       
+
+//        static void Main(string[] args)
+//        {
+//            Trokut br1 = new Trokut();
+//            Console.WriteLine("")
+
+//            Console.WriteLine("Opseg prvog trokuta: " + br1.Opseg());
+//            Console.WriteLine("Površina prvog trokuta: " + br1.Povrsina());
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        }
+
+//        class Trokut
+//        {
+//            public int a, b, c;
+//            public Trokut()
+//            {
+//                a = 3;
+//                b = 4;
+//                c = 5;
+
+
+//            }
+
+//            public Trokut(int a, int b, int c)
+//            {
+//                this.a = a;
+//                this.b = b;
+//                this.c = c;
+//            }
+
+//            public int Opseg()
+//            {
+//                return a + b + c;
+
+//            }
+
+//            public int Povrsina()
+//            {
+//                int pov = (a + b + c) / 2;
+//                return (int)Math.Sqrt(pov * (pov - a) * (pov - b) * (pov - c));
+
+//            }
+
+//            public void Greska()
+//            {
+//                Console.WriteLine("Stranice ne čine trokut");
+//            }
+//        }
+//    }
+
+//}
